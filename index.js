e1c58fa7a0dd885cb826fd3571ee83b5a99911c4
laiba tanveer <shahlaiba152@gmail.com>
2023-08-30 00:40:58 +0500

    FIX

--- index.js
+++ index.js
@@ -2,35 +2,29 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const port = 3000;
+const port = 3030;
+const path = require("path");
 
 const registrationRouter = require("./API/Registration/Router");
 const productRouter = require("./API/Products/Router");
 const categoryRouter = require("./API/Category/Router");
 const brandsRouter = require("./API/Brands/Router");
-const orderRouter =require("./API/Order/router");
-const mailRouter = require("./API/Mailer/Router")
-
-require("dotenv").config();
-console.log(process.env.MONGO_URI);
-const mongoose = require("mongoose");
-
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("DB connected"))
-  .catch((error) => console.error("DB connection error:", error));
+const orderRouter = require("./API/Order/Router");
+const mailRouter = require("./API/Mailer/Router");
 
 app.use(express.json());
 app.use(cors());
+app.use("/", express.static(path.join(__dirname, "./dist")));
 app.use("/api/registration", registrationRouter);
 app.use("/api/products", productRouter);
 app.use("/api/category", categoryRouter);
 app.use("/api/brands", brandsRouter);
 app.use("/api/order", orderRouter);
 app.use("/api/mail", mailRouter);
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "./dist/index.html"))
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
